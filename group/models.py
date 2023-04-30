5b4d980fe42aa41932b51a94e9a22f2b4e8f21a4
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-04-30 23:26:15 +0430

    create group model

--- group/models.py
+++ group/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from accounts.models import User
+from read_book.models import Genre
 # Create your models here.
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=250, blank=True)
+    picture = models.ImageField(default="groupimages/default.jpg", upload_to='groupimages')
+    users = models.ManyToManyField(User, blank=True)
+    category = models.ForeignKey(to=Genre, on_delete=models.CASCADE)
