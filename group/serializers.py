93ac94e7e1cf2c52c48754776ee164ec6a42699b
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-05-01 18:01:57 +0430

    Group serializers

---
+++ group/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Group
+from read_book.models import Genre
+
+
+class CreateGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name', 'bio', 'category', )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'id', )
+
+
+class ShowGroupSerializer(serializers.ModelSerializer):
+    category = GenreSerializer()
+
+    class Meta:
+        model = Group
+        fields = '__all__'
