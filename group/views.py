f6d336b08c7dbdb5986cc60bc871f3d6b05eafa9
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-05-01 18:02:25 +0430

    create and show groups api

--- group/views.py
+++ group/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from .serializers import CreateGroupSerializer, ShowGroupSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Group
 # Create your views here.
+
+
+class CreateGroup(CreateAPIView):
+    serializer_class = CreateGroupSerializer
+    permission_classes = [IsAuthenticated, ]
+
+
+class ShowAllGroups(ListAPIView):
+    serializer_class = ShowGroupSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        return Group.objects.all()
+
+
+class ShowCategoryGroup(ListAPIView):
+    serializer_class = ShowGroupSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        genre_id = self.request.query_params.get('genre_id')
+        return Group.objects.filter(category=genre_id)
